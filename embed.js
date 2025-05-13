c059bab791d244fb50e77c3e532b24670a5833fb
SinanKator <tcstorrance+sinank@gmail.com>
2025-05-12 18:04:45 -0700

    Update embed.js

--- embed.js
+++ embed.js
@@ -4,5 +4,5 @@ window.addEventListener("load", () => {
     player.style.width = "600px";
     player.style.height = "600px";
     document.getElementById("game").appendChild(player);
-    player.load("sumo_ball.swf");
+    player.load("sumoball.swf");
 })
